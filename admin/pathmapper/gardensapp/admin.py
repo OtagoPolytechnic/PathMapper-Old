fd2357d97ae258ba5ead96e3fd39e663e7822818
acraig94 <acraig94@gmail.com>
2016-10-14 12:03:39 +1300

    Removed old backend

--- admin/pathmapper/gardensapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.gis import admin
-
-from .models import Plant, Collection, PlantCollection, Track
-
-admin.site.register(Plant)
-admin.site.register(Collection)
-admin.site.register(PlantCollection)
-admin.site.register(Track, admin.OSMGeoAdmin)
