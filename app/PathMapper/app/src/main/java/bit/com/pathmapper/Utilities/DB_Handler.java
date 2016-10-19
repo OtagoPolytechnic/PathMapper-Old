0c8134252936d1761f8fa7988281d77e61a0fc72
iiSABRE <cameronthomasjackson@gmail.com>
2016-10-20 10:47:51 +1300

    Added utility variables

--- app/PathMapper/app/src/main/java/bit/com/pathmapper/Utilities/DB_Handler.java
+++ app/PathMapper/app/src/main/java/bit/com/pathmapper/Utilities/DB_Handler.java
@@ -1,8 +1,45 @@
 package bit.com.pathmapper.Utilities;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by jacksct1 on 20/10/2016.
  */
 
-public class DB_Handler {
+public class DB_Handler extends SQLiteOpenHelper
+{
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+    // Database Name
+    private static final String DATABASE_NAME = "pathmapperDB";
+    //Table names
+    private static final String TABLE_POI = "tblPOI";
+    private static final String TABLE_COLLECTION = "tblCollection";
+    //POI Table Columns names
+    private static final String KEY_POI_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_SCI_NAME = "scientificName";
+    private static final String KEY_LAT = "lat";
+    private static final String KEY_LNG = "lng";
+    private static final String KEY_DESCRIPTION = "description";
+    private static final String KEY_COLLECTION = "collection";
+    //Collection Columns names
+    private static final String KEY_COLLECTION_ID = "id";
+    private static final String KEY_COLLECTION_NAME = "collectionName";
+
+    public DB_Handler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
