31b90376d43f1128fef482165c48e87dc8530aa5
iiSABRE <cameronthomasjackson@gmail.com>
2016-10-20 10:26:39 +1300

    Collection model complete
    Fields and Getter/setters

--- app/PathMapper/app/src/main/java/bit/com/pathmapper/Models/Collection.java
+++ app/PathMapper/app/src/main/java/bit/com/pathmapper/Models/Collection.java
@@ -4,5 +4,32 @@ package bit.com.pathmapper.Models;
  * Created by jacksct1 on 20/10/2016.
  */
 
-public class Collection {
+public class Collection
+{
+    private int id;
+    private int collectionName;
+
+    public Collection(int id, int collectionName) {
+        this.id = id;
+        this.collectionName = collectionName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCollectionName() {
+        return collectionName;
+    }
+
+    public void setCollectionName(int collectionName) {
+        this.collectionName = collectionName;
+    }
 }
+
+
+
